ce8ccf477f7c499fc80d50a41f5b2270a50c22cd
OGPasindu <ogpmadhuwantha678@gmail.com>
2023-10-22 19:30:19 +0530

    #

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-var bg = new Audio("pubg_login.mp3");
-bg.loop = true;
+var bg1 = new Audio("PUBG_Login.mp3");
+bg1.loop = true;
+var bg2 = new Audio("PUBG_Register.mp3")
+bg2.loop = true;
 
 function login() {
-    bg.play();
+    bg1.play();
+    bg2.pause();
+}
+
+function register() {
+    bg2.play();
+    bg1.pause();
+}
+
+function change() {
+    var loginbox = document.getElementById("loginbox");
+    var registerbox = document.getElementById("registerbox");
+
+    loginbox.classList.toggle("d-none");
+    registerbox.classList.toggle("d-none");
+
 }
